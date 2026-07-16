51b70fe66b73bfcfbd03cf325bfd5e454cb6975b
artem <kebabggbab@gmail.com>
2026-07-16 19:50:54 +0700

    feat(shared): добавлен базовый класс для доменных исключений

---
+++ src/SharedKernel/TestTask.Timescale.SharedKernel.Domain/Exceptions/DomainException .cs
@@ -0,0 +1,17 @@
+﻿namespace TestTask.Timescale.SharedKernel.Domain.Exceptions
+{
+    public abstract class DomainException : Exception
+    {
+        public DomainException()
+        {
+        }
+
+        public DomainException(string? message) : base(message)
+        {
+        }
+
+        public DomainException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
