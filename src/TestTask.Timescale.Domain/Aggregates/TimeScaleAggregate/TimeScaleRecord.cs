dc728db2fe9c984f976a8a18279ce86eafffc21d
artem <kebabggbab@gmail.com>
2026-07-17 23:51:54 +0700

    feat(domain): добавлен класс TimeScaleRecord, представляющий строку в csv файле

---
+++ src/TestTask.Timescale.Domain/Aggregates/TimeScaleAggregate/TimeScaleRecord.cs
@@ -0,0 +1,52 @@
+﻿using TestTask.Timescale.SharedKernel.Domain.BaseModels;
+using TestTask.Timescale.SharedKernel.Domain.Results;
+
+namespace TestTask.Timescale.Domain.Aggregates.TimeScaleAggregate
+{
+    public class TimeScaleRecord : Entity
+    {
+        public Timestamp Date { get; }
+
+        public ExecutionTime Time { get; }
+
+        public TimeScaleRecordValue Value { get; }
+
+        private TimeScaleRecord(Timestamp date, ExecutionTime time, TimeScaleRecordValue value)
+        {
+            Date = date;
+            Time = time;
+            Value = value;
+        }
+
+        public static Result CanCreate(DateTime date, double seconds, double value)
+        {
+            Result timestamp = Timestamp.CanCreate(date);
+            Result time = ExecutionTime.CanCreate(seconds);
+            Result unitValue = TimeScaleRecordValue.CanCreate(value);
+
+            if (timestamp.IsFailure || time.IsFailure || unitValue.IsFailure)
+            {
+                return Result.Fail(timestamp.Errors
+                    .Concat(time.Errors)
+                    .Concat(unitValue.Errors));
+            }
+
+            return Result.Ok();
+        }
+
+        public static Result<TimeScaleRecord> Create(DateTime date, double seconds, double value)
+        {
+            Result canCreate = CanCreate(date, seconds, value);
+
+            if (canCreate.IsFailure)
+            {
+                return Result.Fail<TimeScaleRecord>(canCreate.Errors);
+            }
+
+            return Result.Ok(new TimeScaleRecord(
+                Timestamp.Create(date).Value, 
+                ExecutionTime.Create(seconds).Value, 
+                TimeScaleRecordValue.Create(value).Value));
+        }
+    }
+}
---
+++ tests/TestTask.Timescale.Domain.Test/TestClasses/TimeScaleRecordTest.cs
@@ -0,0 +1,64 @@
+﻿using TestTask.Timescale.Domain.Aggregates.TimeScaleAggregate;
+using TestTask.Timescale.SharedKernel.Domain.Results;
+
+namespace TestTask.Timescale.Domain.Test.TestClasses
+{
+    [TestClass]
+    public class TimeScaleRecordTest
+    {
+        [TestMethod]
+        public void CanCreate_AllPropertiesValid_Success()
+        {
+            DateTime date = new(2020, 01, 01);
+            double seconds = 2.3d;
+            double value = 2.3d;
+
+            Result result = TimeScaleRecord.CanCreate(date, seconds, value);
+
+            Assert.IsTrue(result.IsSuccess);
+        }
+
+        [TestMethod]
+        [DataRow(2020, 2.3, -1)]
+        [DataRow(2020, -1, 1)]
+        [DataRow(1999, 2.3, 1)]
+        public void CanCreate_SomePropertiesNotValid_Fail(int year, double seconds, double value)
+        {
+            DateTime date = new(year, 01, 01);
+
+            Result result = TimeScaleRecord.CanCreate(date, seconds, value);
+
+            Assert.IsTrue(result.IsFailure);
+        }
+
+        [TestMethod]
+        public void Create_AllPropertiesValid_ObjectWithCorrectValueObject()
+        {
+            DateTime date = new(2020, 01, 01);
+            double seconds = 2.3d;
+            double value = 2.3d;
+            Timestamp expectedDate = Timestamp.Create(date).Value;
+            ExecutionTime expectedSeconds = ExecutionTime.Create(seconds).Value;
+            TimeScaleRecordValue expectedValue = TimeScaleRecordValue.Create(value).Value;
+
+            Result<TimeScaleRecord> result = TimeScaleRecord.Create(date, seconds, value);
+
+            Assert.AreEqual(expectedDate, result.Value.Date);
+            Assert.AreEqual(expectedSeconds, result.Value.Time);
+            Assert.AreEqual(expectedValue, result.Value.Value);
+        }
+
+        [TestMethod]
+        [DataRow(2020, 2.3, -1)]
+        [DataRow(2020, -1, 1)]
+        [DataRow(1999, 2.3, 1)]
+        public void Create_SomePropertiesNotValid_Fail(int year, double seconds, double value)
+        {
+            DateTime date = new(year, 01, 01);
+
+            Result<TimeScaleRecord> result = TimeScaleRecord.Create(date, seconds, value);
+
+            Assert.IsTrue(result.IsFailure);
+        }
+    }
+}
