094a778254bc9f0029e546849dcaef50e6835345
artem <kebabggbab@gmail.com>
2026-07-17 16:43:31 +0700

    feat(shared): добавлен класс AggregateRoot, представляющий на данный момент просто метку

---
+++ src/SharedKernel/TestTask.Timescale.SharedKernel.Domain/BaseModels/AggregateRoot.cs
@@ -0,0 +1,8 @@
+﻿namespace TestTask.Timescale.SharedKernel.Domain.BaseModels
+{
+    public abstract class AggregateRoot : Entity
+    {
+        // На данный момент не требуются события или что-либо еще, что необходимо
+        // поместить в корень агрегата, поэтому сейчас этот класс просто маркер
+    }
+}
