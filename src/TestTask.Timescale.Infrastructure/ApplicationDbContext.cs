13725555d54272051169b2da0c3d1120c37ed553
artem <kebabggbab@gmail.com>
2026-07-21 05:21:37 +0700

    feat(infrastructure): добавлен контекст бд - ApplicationDbContext

---
+++ src/TestTask.Timescale.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Timescale.Domain.Aggregates.MetricsAggregate;
+using TestTask.Timescale.Domain.Aggregates.RecordAggregate;
+using TestTask.Timescale.Domain.Aggregates.TimeScaleAggregate;
+using TestTask.Timescale.Infrastructure.EntityTypeConfigurations;
+
+namespace TestTask.Timescale.Infrastructure
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<TimeScale> TimeScales { get; set; }
+
+        public DbSet<Record> Records { get; set; }
+
+        public DbSet<Metrics> Metrics { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseIdentityAlwaysColumns();
+
+            modelBuilder.ApplyConfiguration(new TimeScaleEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new RecordEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new MetricsEntityTypeConfiguration());
+        }
+    }
+}
