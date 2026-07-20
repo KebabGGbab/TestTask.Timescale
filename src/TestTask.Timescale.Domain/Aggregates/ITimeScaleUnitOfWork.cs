4b357d386cd33eb0ef87b87c73692c459f3dc292
artem <kebabggbab@gmail.com>
2026-07-21 05:15:26 +0700

    feat(domain): добавлен ITimeScaleUnitOfWork расширяющий IUnitOfWork для конкретного домена

---
+++ src/TestTask.Timescale.Domain/Aggregates/ITimeScaleUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using TestTask.Timescale.Domain.Aggregates.MetricsAggregate;
+using TestTask.Timescale.Domain.Aggregates.RecordAggregate;
+using TestTask.Timescale.Domain.Aggregates.TimeScaleAggregate;
+using TestTask.Timescale.SharedKernel.Domain.Data;
+
+namespace TestTask.Timescale.Domain.Aggregates
+{
+    public interface ITimeScaleUnitOfWork : IUnitOfWork
+    {
+        ITimeScaleRepository TimeScaleRepository { get; }
+
+        IRecordRepository RecordRepository { get; }
+
+        IMetricsRepository MetricsRepository { get; }
+    }
+}
