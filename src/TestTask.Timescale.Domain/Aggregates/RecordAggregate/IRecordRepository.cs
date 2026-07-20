2c5320fa11af5035f0cfe6eea44bc5f0a7de192f
artem <kebabggbab@gmail.com>
2026-07-21 05:14:23 +0700

    feat(domain): в IRecordRepository добавлены методы Add и GetRecordsByFileNameAsync

--- src/TestTask.Timescale.Domain/Aggregates/RecordAggregate/IRecordRepository.cs
+++ src/TestTask.Timescale.Domain/Aggregates/RecordAggregate/IRecordRepository.cs
@@ -2,7 +2,11 @@
 {
     public interface IRecordRepository
     {
+        void Add(Record record);
+
         void Add(IEnumerable<Record> records);
+
+        Task<IEnumerable<Record>> GetRecordsByFileNameAsync(string fileName, CancellationToken cancellation = default);
 
         Task<IEnumerable<Record>> GetLastByFileNameAndOrderByDateAsync(string fileName, int count, CancellationToken cancellation = default);
     }
