279eacc071034ba548f02a9c88ca9187aacc08fc
artem <kebabggbab@gmail.com>
2026-07-21 06:55:21 +0700

    feat(infrastructure): добавлена реализация TimeScaleUnitOfWork

---
+++ src/TestTask.Timescale.Infrastructure/Repositories/TimeScaleUnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using TestTask.Timescale.Domain.Aggregates;
+using TestTask.Timescale.Domain.Aggregates.MetricsAggregate;
+using TestTask.Timescale.Domain.Aggregates.RecordAggregate;
+using TestTask.Timescale.Domain.Aggregates.TimeScaleAggregate;
+using TestTask.Timescale.SharedKernel.Domain.Events;
+using TestTask.Timescale.SharedKernel.Infrastructure;
+
+namespace TestTask.Timescale.Infrastructure.Repositories
+{
+    public class TimeScaleUnitOfWork : UnitOfWork, ITimeScaleUnitOfWork
+    {
+        public ITimeScaleRepository TimeScaleRepository { get; }
+        
+        public IRecordRepository RecordRepository { get; }
+
+        public IMetricsRepository MetricsRepository { get; }
+
+        public TimeScaleUnitOfWork(ApplicationDbContext db, IDomainEventDispatcher eventDispatcher) 
+            : base(db, eventDispatcher)
+        {
+            TimeScaleRepository = new TimeScaleRepository(db);
+            RecordRepository = new RecordRepository(db);
+            MetricsRepository = new MetricsRepository(db);
+        }
+    }
+}
