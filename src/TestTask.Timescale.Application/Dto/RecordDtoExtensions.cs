405e1388ebf9919648afda6346b448de065d2938
artem <kebabggbab@gmail.com>
2026-07-21 23:48:53 +0700

    feat(application): расширения для RecordDto

---
+++ src/TestTask.Timescale.Application/Dto/RecordDtoExtensions.cs
@@ -0,0 +1,19 @@
+﻿using TestTask.Timescale.Domain.Aggregates.RecordAggregate;
+using TestTask.Timescale.Domain.Dto;
+
+namespace TestTask.Timescale.Application.Dto
+{
+    public static class RecordDtoExtensions
+    {
+        extension(RecordDto)
+        {
+            public static RecordDto From(Record record)
+            {
+                return new RecordDto(
+                    record.Date.Value,
+                    record.Time.Seconds,
+                    record.Value.Indicator);
+            }
+        }
+    }
+}
