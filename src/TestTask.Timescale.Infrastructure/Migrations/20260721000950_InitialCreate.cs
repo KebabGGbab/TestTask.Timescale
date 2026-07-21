aac2594ee4ec0bdeb47b058762bd7c6830be7177
artem <kebabggbab@gmail.com>
2026-07-21 07:14:10 +0700

    feat(infrastructure): добавлена миграция инициализации бд

---
+++ src/TestTask.Timescale.Infrastructure/Migrations/20260721000950_InitialCreate.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace TestTask.Timescale.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "time_scales",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn),
+                    file_name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_time_scales", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "result",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn),
+                    time_scale_id = table.Column<int>(type: "integer", nullable: false),
+                    delta_date = table.Column<double>(type: "double precision", nullable: false),
+                    min_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    avg_execution_duration = table.Column<double>(type: "double precision", nullable: false),
+                    avg_value = table.Column<double>(type: "double precision", nullable: false),
+                    median_value = table.Column<double>(type: "double precision", nullable: false),
+                    max_value = table.Column<double>(type: "double precision", nullable: false),
+                    min_value = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_result", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_result_time_scales_time_scale_id",
+                        column: x => x.time_scale_id,
+                        principalTable: "time_scales",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "values",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn),
+                    time_scale_id = table.Column<int>(type: "integer", nullable: false),
+                    date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    execution_time = table.Column<double>(type: "double precision", nullable: false),
+                    value = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_values", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_values_time_scales_time_scale_id",
+                        column: x => x.time_scale_id,
+                        principalTable: "time_scales",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_result_time_scale_id",
+                table: "result",
+                column: "time_scale_id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_values_time_scale_id",
+                table: "values",
+                column: "time_scale_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "result");
+
+            migrationBuilder.DropTable(
+                name: "values");
+
+            migrationBuilder.DropTable(
+                name: "time_scales");
+        }
+    }
+}
