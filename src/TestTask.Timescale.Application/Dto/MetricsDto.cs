9a348c34185edae8fca7911c33fc8f8e5ce369b6
artem <kebabggbab@gmail.com>
2026-07-21 23:48:28 +0700

    feat(application): добавлено MetricsDto

---
+++ src/TestTask.Timescale.Application/Dto/MetricsDto.cs
@@ -0,0 +1,37 @@
+﻿namespace TestTask.Timescale.Application.Dto
+{
+    public class MetricsDto
+    {
+        public int Id { get; }
+
+        public string FileName { get; }
+
+        public double DeltaDate { get; }
+
+        public DateTime MinDate { get; }
+
+        public double AvgExecutionDuration { get; }
+
+        public double AvgValue { get; }
+
+        public double MedianValue { get; }
+
+        public double MaxValue { get; }
+
+        public double MinValue { get; }
+
+        public MetricsDto(int id, string fileName, double deltaDate, DateTime minDate, double avgExecutionDuration,
+            double avgValue, double medianValue, double maxValue, double minValue)
+        {
+            Id = id;
+            FileName = fileName;
+            DeltaDate = deltaDate;
+            MinDate = minDate;
+            AvgExecutionDuration = avgExecutionDuration;
+            AvgValue = avgValue;
+            MedianValue = medianValue;
+            MaxValue = maxValue;
+            MinValue = minValue;
+        }
+    }
+}
---
+++ src/TestTask.Timescale.Application/Dto/MetricsDtoExtensions.cs
@@ -0,0 +1,24 @@
+﻿using TestTask.Timescale.Domain.Aggregates.MetricsAggregate;
+
+namespace TestTask.Timescale.Application.Dto
+{
+    public static class MetricsDtoExtensions
+    {
+        extension(MetricsDto)
+        {
+            public static MetricsDto From(Metrics metrics, string fileName)
+            {
+                return new MetricsDto(
+                    metrics.Id,
+                    fileName,
+                    metrics.DeltaDate,
+                    metrics.MinDate,
+                    metrics.AvgExecutionDuration,
+                    metrics.AvgValue,
+                    metrics.MedianValue,
+                    metrics.MaxValue,
+                    metrics.MinValue);
+            }
+        }
+    }
+}
