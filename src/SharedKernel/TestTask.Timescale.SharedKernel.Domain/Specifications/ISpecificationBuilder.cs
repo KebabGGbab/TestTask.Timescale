85e529137bb175cac84be687458ab3dfb1f25896
artem <kebabggbab@gmail.com>
2026-07-23 04:32:10 +0700

    feat(shared): в shared domain добавлен интерфейс строителя спецификаций

---
+++ src/SharedKernel/TestTask.Timescale.SharedKernel.Domain/Specifications/ISpecificationBuilder.cs
@@ -0,0 +1,9 @@
+﻿namespace TestTask.Timescale.SharedKernel.Domain.Specifications
+{
+    public interface ISpecificationBuilder<T>
+    {
+        ISpecificationBuilder<T> And(ISpecification<T> specification);
+
+        ISpecification<T>? Build();
+    }
+}
