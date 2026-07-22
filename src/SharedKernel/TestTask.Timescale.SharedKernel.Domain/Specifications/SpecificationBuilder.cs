4e1a3b9286ee18e9cd1074491aeeef5cd4908ad2
artem <kebabggbab@gmail.com>
2026-07-23 04:33:03 +0700

    feat(shared): в shared domain добавлена реализация ISpecificationBuilder

---
+++ src/SharedKernel/TestTask.Timescale.SharedKernel.Domain/Specifications/SpecificationBuilder.cs
@@ -0,0 +1,21 @@
+﻿namespace TestTask.Timescale.SharedKernel.Domain.Specifications
+{
+    public class SpecificationBuilder<T> : ISpecificationBuilder<T>
+    {
+        private ISpecification<T>? _currentSpecification;
+
+        public ISpecificationBuilder<T> And(ISpecification<T> specification)
+        {
+            _currentSpecification = _currentSpecification == null
+                ? specification
+                : _currentSpecification.And(specification);
+
+            return this;
+        }
+
+        public ISpecification<T>? Build()
+        {
+            return _currentSpecification;
+        }
+    }
+}
